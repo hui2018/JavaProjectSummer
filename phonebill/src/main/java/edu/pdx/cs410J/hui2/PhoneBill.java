5b85584725274a8f9b1f2f4248eb438125ba92ee
Jack <hui2@pdx.edu>
2018-06-30 16:18:44 -0700

    Add new phonebill class -retrial

---
+++ phonebill/src/main/java/edu/pdx/cs410J/hui2/PhoneBill.java
@@ -0,0 +1,24 @@
+package edu.pdx.cs410J.hui2;
+
+import edu.pdx.cs410J.AbstractPhoneBill;
+import edu.pdx.cs410J.AbstractPhoneCall;
+
+import java.util.Collection;
+
+public class PhoneBill extends AbstractPhoneBill {
+
+    @Override
+    public String getCustomer() {
+        return null;
+    }
+
+    @Override
+    public void addPhoneCall(AbstractPhoneCall call) {
+
+    }
+
+    @Override
+    public Collection getPhoneCalls() {
+        return null;
+    }
+}
